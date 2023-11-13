e5c33744b90fcb7b43d67a57b453875000117896
admeneses <alejandrocardie12@gmail.com>
2023-11-12 22:10:12 -0400

    add qn2p7

--- HW-1/qn2.hs
+++ HW-1/qn2.hs
@@ -3,23 +3,23 @@ data ArbolMB a = Vacio
                | RamaM a (ArbolMB a)
                | RamaB a (ArbolMB a) (ArbolMB a)
 
--- Constructores de datos (solo tipos)
---vacio :: ArbolMB a
---
---ramaM :: a -> ArbolMB a -> ArbolMB a
---
---ramaB :: a -> ArbolMB a -> ArbolMB a -> ArbolMB a
---
----- Transforma valores de tipo (ArbolMB a) en algun otro tipo b.
----- Se supone que cada argumento de tipo (ArbolMB a) ya ha sido
----- transformado a b
---transformarRamaM :: a -> b -> b
---
---transformarRamaB :: a -> b -> b -> b
---
---transformarVacio :: b
+-- a) Constructores de datos (solo tipos)
+vacio :: ArbolMB a
 
--- Funcion transformadora
+ramaM :: a -> ArbolMB a -> ArbolMB a
+
+ramaB :: a -> ArbolMB a -> ArbolMB a -> ArbolMB a
+
+--b) Transforma valores de tipo (ArbolMB a) en algun otro tipo b.
+-- Se supone que cada argumento de tipo (ArbolMB a) ya ha sido
+-- transformado a b
+transformarRamaM :: a -> b -> b
+
+transformarRamaB :: a -> b -> b -> b
+
+transformarVacio :: b
+
+-- c) Funcion transformadora
 plegarArbolMB :: (b) 
               -> (a -> b -> b)
               -> (a -> b -> b -> b)
@@ -31,7 +31,7 @@ plegarArbolMB transVacio transRamaM transRamaB = plegar
     plegar (RamaM x y) = transRamaM x (plegar y)
     plegar (RamaB x y z) = transRamaB x (plegar y) (plegar z)
 
--- Dado un valor de tipo (Num a) => ArbolMB a calcula y retorna la 
+-- d) Dado un valor de tipo (Num a) => ArbolMB a calcula y retorna la 
 -- suma de todos datos almacenados en el tipo
 sumarArbolMB :: (Num a) => ArbolMB a -> a
 sumarArbolMB = plegarArbolMB transVacio transRamaM transRamaB
@@ -40,7 +40,7 @@ sumarArbolMB = plegarArbolMB transVacio transRamaM transRamaB
     transRamaM x y = x + y
     transRamaB x y z = x + y + z
 
--- Dado un valor de tipo ArbolMB a calcula y retorna una sola lista
+-- e) Dado un valor de tipo ArbolMB a calcula y retorna una sola lista
 -- con todos los elementos contenidos en la estructura (en el orden
 -- in-order)
 aplanarArbolMB :: ArbolMB a -> [a]
@@ -50,12 +50,12 @@ aplanarArbolMB = plegarArbolMB transVacio transRamaM transRamaB
     transRamaM x y = y ++ [x]
     transRamaB x y z = y ++ [x] ++ z
 
---  Dado un valor de tipo (Ord a) => ArbolMB a calcula y retorna
---  posiblemente una tupla con 3 elementos. El primero es el minimo
---  elemento presente en la estructura, el segundo es el maximo y el
---  tercero es un booleano que es cierto si y solo si la lista que
---  resultaria de llamar a la funcion aplanarArbolMB estaria ordenada
---  de menor a mayor (esto es, que sea un arbol de busqueda)
+-- f) Dado un valor de tipo (Ord a) => ArbolMB a calcula y retorna
+-- posiblemente una tupla con 3 elementos. El primero es el minimo
+-- elemento presente en la estructura, el segundo es el maximo y el
+-- tercero es un booleano que es cierto si y solo si la lista que
+-- resultaria de llamar a la funcion aplanarArbolMB estaria ordenada
+-- de menor a mayor (esto es, que sea un arbol de busqueda)
 analizarArbolMB :: (Ord a) => ArbolMB a -> Maybe (a, a, Bool)
 analizarArbolMB = plegarArbolMB transVacio transRamaM transRamaB
   where
@@ -69,7 +69,12 @@ analizarArbolMB = plegarArbolMB transVacio transRamaM transRamaB
           isSorted = isSortedLeft && isSortedRight && (v >= lmax) && (v <= rmin)
       return (xmin, xmax, isSorted)
 
-    
+-- g) Para un tipo de datos mas general Gen a, con n constructores diferentes.
+-- Si se quisiera crear una funcion plegarGen, con un comportamiento similar 
+-- al de plegarArbolMB, la funcion deberia tomar como argumentos una funcion
+-- por cada uno de los n constructores, demás del valor de tipo Gen a que se
+-- desea plegar.
+
 -- Test
 --main = do
 --  let arbol = RamaB 5 (RamaB 3 (RamaB 2 Vacio Vacio) (RamaB 4 Vacio Vacio)) (RamaB 7 (RamaB 6 Vacio Vacio) (RamaB 8 Vacio Vacio))
