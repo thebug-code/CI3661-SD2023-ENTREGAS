1b8243ae92265e5f3b6470dcd1839483e0c13925
admeneses <alejandrocardie12@gmail.com>
2023-11-28 16:08:20 -0400

    add 2.c

--- HW-2/moneda.rb
+++ HW-2/moneda.rb
@@ -2,10 +2,10 @@
 # Tasas de cambio
 $USD_TO_JPY_EXR = 148.225
 $USD_TO_EUR_EXR = 0.912747
-$USD_TO_VED_EXR = 3542580
+$USD_TO_VED_EXR = 35.4784
 $USD_TO_BTC_EXR = 0.0000269283
 $EUR_TO_JPY_EXR = 162.366
-$EUR_TO_VED_EXR = 3880550
+$EUR_TO_VED_EXR = 38.92015958
 $EUR_TO_BTC_EXR = 0.0000294921
 $VED_TO_JPY_EXR = 0.0000418389
 $VED_TO_BTC_EXR = 0.00000000000759570
@@ -19,59 +19,129 @@ class Moneda
   def en(coin_sym)
     send(coin_sym)
   end
+
+  def compare_two_coins(c1, c2)
+    if c1 > c2
+      return :mayor
+    elsif c1 < c2
+      return :menor
+    else
+      return :igual
+    end
+  end
 end
 
 class Dolar < Moneda
   def yens
-    @cnt * $USD_TO_JPY_EXR
+    (@cnt * $USD_TO_JPY_EXR).round(2)
   end
   
   def euros
-    @cnt * $USD_TO_EUR_EXR
+    (@cnt * $USD_TO_EUR_EXR).round(2)
   end
 
   def bolivares
-    @cnt * $USD_TO_VED_EXR
+    (@cnt * $USD_TO_VED_EXR).round(2)
   end
 
   def bitcoins
-    @cnt * $USD_TO_BTC_EXR
+    (@cnt * $USD_TO_BTC_EXR).round(2)
+  end
+
+  def comparar(other)
+    other.compare_wth_dolar(self)
+  end
+
+  def compare_wth_yen(yen)
+    compare_two_coins(yen.dolares, @cnt)
+  end
+
+  def compare_wth_euro(euro)
+    compare_two_coins(euro.dolares, @cnt)
+  end
+
+  def compare_wth_bolivar(bolivar)
+    compare_two_coins(bolivar.dolares, @cnt)
+  end
+
+  def compare_wth_bitcoin(bitcoin)
+    compare_two_coins(bitcoin.dolares, @cnt)
   end
 end
 
 class Yen < Moneda
   def dolares
-    @cnt / $USD_TO_JPY_EXR
+    (@cnt / $USD_TO_JPY_EXR).round(2)
   end
 
   def euros
-    @cnt / $EUR_TO_JPY_EXR
+    (@cnt / $EUR_TO_JPY_EXR).round(2)
   end
 
   def bolivares
-    @cnt / $VED_TO_JPY_EXR
+    (@cnt / $VED_TO_JPY_EXR).round(2)
   end
 
   def bitcoins
-    @cnt * $JPY_TO_BTC_EXR
+    (@cnt * $JPY_TO_BTC_EXR).round(2)
+  end
+
+  def comparar(other)
+    other.compare_wth_yen(self)
+  end
+
+  def compare_wth_dolar(dolar)
+    compare_two_coins(dolar.yens, @cnt)
+  end
+
+  def compare_wth_euro(euro)
+    compare_two_coins(euro.yens, @cnt)
+  end
+
+  def compare_wth_bolivar(bolivar)
+    compare_two_coins(bolivar.yens, @cnt)
+  end
+
+  def compare_wth_bitcoin(bitcoin)
+    compare_two_coins(bitcoin.yens, @cnt)
   end
 end
 
 class Euro < Moneda
   def dolares
-    @cnt / $USD_TO_EUR_EXR
+    (@cnt / $USD_TO_EUR_EXR).round(2)
   end
 
   def bolivares
-    @cnt * $EUR_TO_VED_EXR
+    (@cnt * $EUR_TO_VED_EXR).round(2)
   end
 
   def yens
-    @cnt * $EUR_TO_JPY_EXR
+    (@cnt * $EUR_TO_JPY_EXR).round(2)
   end
 
   def bitcoins
-    @cnt * $EUR_TO_BTC_EXR
+    (@cnt * $EUR_TO_BTC_EXR).round(2)
+  end
+
+  def comparar(other)
+    other.compare_wth_euro(self)
+  end
+
+  def compare_wth_dolar(dolar)
+    compare_two_coins(dolar.euros, @cnt)
+  end
+
+  def compare_wth_yen(yen)
+    compare_two_coins(yen.euros, @cnt)
+  end
+
+  def compare_wth_bolivar(bolivar)
+    compare_two_coins(bolivar.euros, @cnt)
+  end
+
+  def compare_wth_bitcoin(bitcoin)
+    compare_two_coins(bitcoin.euros, @cnt)
   end
 end
 
@@ -91,6 +161,26 @@ class Bolivar < Moneda
   def bitcoins
     @cnt * $VED_TO_BTC_EXR
   end
+
+  def comparar(other)
+    other.compare_wth_bolivar(self)
+  end
+
+  def compare_wth_dolar(dolar)
+    compare_two_coins(dolar.bolivares, @cnt)
+  end
+
+  def compare_wth_yen(yen)
+    compare_two_coins(yen.bolivares, @cnt)
+  end
+
+  def compare_wth_euro(euro)
+    compare_two_coins(euro.bolivares, @cnt)
+  end
+
+  def compare_wth_bitcoin(bitcoin)
+    compare_two_coins(bitcoin.bolivares, @cnt)
+  end
 end
 
 class Bitcoin < Moneda
@@ -108,6 +198,26 @@ class Bitcoin < Moneda
 
   def bolivares
     @cnt / $VED_TO_BTC_EXR
+  end
+
+  def comparar(other)
+    other.compare_wth_bitcoin(self)
+  end
+
+  def compare_wth_dolar(dolar)
+    compare_two_coins(dolar.bitcoins, @cnt)
+  end
+
+  def compare_wth_yen(yen)
+    compare_two_coins(yen.bitcoins, @cnt)
+  end 
+
+  def compare_wth_euro(euro)
+    compare_two_coins(euro.bitcoins, @cnt)
+  end
+
+  def compare_wth_bolivar(bolivar)
+    compare_two_coins(bolivar.bitcoins, @cnt)
   end
 end
 
