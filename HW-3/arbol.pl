39051fbfef92df2d9ad0f2111c90ea1c3d719dc5
admeneses <alejandrocardie12@gmail.com>
2023-12-08 16:35:53 -0400

    add 2.b

--- HW-3/arbol.pl
+++ HW-3/arbol.pl
@@ -1,6 +1,11 @@
 % Hechos
-arco('C', 'A').
-arco('C', 'C').
+arco('a', 'b').
+arco('b', 'c').
 
 % Define el predicado hermano
 hermano(A, B) :- arco(C, A), arco(C, B).
+
+% Define el predicado alcanzable
+alcanzable(A, A).
+alcanzable(A, B) :- arco(A, B).
+alcanzable(A, B) :- arco(A, C), alcanzable(C, B).
