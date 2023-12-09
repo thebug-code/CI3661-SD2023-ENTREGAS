c3594df9e0f143f529b71dc2f149aa5f3314fc9d
admeneses <alejandrocardie12@gmail.com>
2023-12-08 23:25:11 -0400

    lista p2 (creo xdxd)

--- HW-3/arbol.pl
+++ HW-3/arbol.pl
@@ -1,10 +1,8 @@
 % Hechos que representan los arcos del grafo
 % arco(X, Y) significa que existe un arco desde X hasta Y
-%arco('c', 'a').
-%arco('c', 'b').
-%arco('c1', 'b').
-%arco('c1', 'a').
-%arco('c', 'c1').
+arco(a, b).
+arco(b, c).
+arco(c, d).
 
 % a.
 
@@ -15,8 +13,8 @@ hermano(A, B) :- arco(C, A), arco(C, B), A \= B.
 
 % B es alcanzable desde A si existe un arco de A a B, o si existe un C
 % tal que existe un arco de A a C y B es alcanzable desde C
-alcanzable(A, B) :- arco(A, B).
-alcanzable(A, B) :- arco(A, C), alcanzable(C, B).
+alcanzable(A, B) :- arco(A, B), \+ alcanzable(A, B).
+alcanzable(A, B) :- arco(A, C), alcanzable(C, B), \+ alcanzable(A, B).
 
 % c.
 
@@ -26,3 +24,31 @@ lca(A, B, C) :-
     alcanzable(C, A),
     alcanzable(C, B),
     not((alcanzable(C, C1), alcanzable(C1, A), alcanzable(C1, B))).
+
+% d.
+
+% Indica si el grafo es un arbol
+tree(A) :- 
+    not(ciclo(A)),         % No hay ciclos
+    not(alcanzable(_, A)), % No hay algun nodo que alcance a A
+    arborescence(A).       % Es una arborescencia
+
+% Indica si el grafo tiene un ciclo
+ciclo(X) :- ciclo(X, []).
+
+ciclo(Curr, Visited) :-
+    member(Curr, Visited),
+    !. 
+
+ciclo(Curr, Visited) :-
+    arco(Curr, Next),
+    ciclo(Next, [Curr|Visited]) .
+
+% Indica si el grafo es una arborescencia
+arborescence(A) :-
+    findall(B, arco(A, B), Hijos),
+    forall(member(H, Hijos), arborescence(H)).
+
+
+
+% https://stackoverflow.com/questions/6721139/cycle-detection-in-a-graph
